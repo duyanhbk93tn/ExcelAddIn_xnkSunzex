2e807bef82dd42cd08d116bf14c53e1c9e085546
Duy Anh Bui Khanh <duyanhbk93tn@gmail.com>
2020-08-31 16:14:04 +0700

    init prj

---
+++ ribbon_xnk_sunzex/ribbon_xnk_sunzex/Ribbon1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace ribbon_xnk_sunzex
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            this.tab_xnk = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.editBox_hoadonmau = this.Factory.CreateRibbonEditBox();
+            this.separator2 = this.Factory.CreateRibbonSeparator();
+            this.editBox_thumucxuat = this.Factory.CreateRibbonEditBox();
+            this.group_xnk = this.Factory.CreateRibbonGroup();
+            this.editBox3 = this.Factory.CreateRibbonEditBox();
+            this.checkBox_openAfter = this.Factory.CreateRibbonCheckBox();
+            this.checkBox_xuatRieng = this.Factory.CreateRibbonCheckBox();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.editBox4 = this.Factory.CreateRibbonEditBox();
+            this.editBox5 = this.Factory.CreateRibbonEditBox();
+            this.checkBox1 = this.Factory.CreateRibbonCheckBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button7 = this.Factory.CreateRibbonButton();
+            this.button_invoice = this.Factory.CreateRibbonButton();
+            this.button_shipping = this.Factory.CreateRibbonButton();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.tab_xnk.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group_xnk.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab_xnk
+            // 
+            this.tab_xnk.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab_xnk.Groups.Add(this.group1);
+            this.tab_xnk.Groups.Add(this.group_xnk);
+            this.tab_xnk.Groups.Add(this.group2);
+            this.tab_xnk.Label = "XNK Sunzex";
+            this.tab_xnk.Name = "tab_xnk";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button5);
+            this.group1.Items.Add(this.editBox_hoadonmau);
+            this.group1.Items.Add(this.separator2);
+            this.group1.Items.Add(this.button7);
+            this.group1.Items.Add(this.editBox_thumucxuat);
+            this.group1.Name = "group1";
+            // 
+            // editBox_hoadonmau
+            // 
+            this.editBox_hoadonmau.Label = " ";
+            this.editBox_hoadonmau.Name = "editBox_hoadonmau";
+            this.editBox_hoadonmau.ShowLabel = false;
+            this.editBox_hoadonmau.Text = null;
+            // 
+            // separator2
+            // 
+            this.separator2.Name = "separator2";
+            // 
+            // editBox_thumucxuat
+            // 
+            this.editBox_thumucxuat.Label = " ";
+            this.editBox_thumucxuat.Name = "editBox_thumucxuat";
+            this.editBox_thumucxuat.ShowLabel = false;
+            this.editBox_thumucxuat.Text = null;
+            // 
+            // group_xnk
+            // 
+            this.group_xnk.Items.Add(this.button_invoice);
+            this.group_xnk.Items.Add(this.button_shipping);
+            this.group_xnk.Items.Add(this.editBox3);
+            this.group_xnk.Items.Add(this.checkBox_openAfter);
+            this.group_xnk.Items.Add(this.checkBox_xuatRieng);
+            this.group_xnk.Label = "Xuất";
+            this.group_xnk.Name = "group_xnk";
+            // 
+            // editBox3
+            // 
+            this.editBox3.Enabled = false;
+            this.editBox3.Label = "Packing list";
+            this.editBox3.Name = "editBox3";
+            this.editBox3.Text = null;
+            // 
+            // checkBox_openAfter
+            // 
+            this.checkBox_openAfter.Label = "Mở file sau khi tạo";
+            this.checkBox_openAfter.Name = "checkBox_openAfter";
+            this.checkBox_openAfter.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.CheckBox3_Click);
+            // 
+            // checkBox_xuatRieng
+            // 
+            this.checkBox_xuatRieng.Label = "Xuất file riêng";
+            this.checkBox_xuatRieng.Name = "checkBox_xuatRieng";
+            this.checkBox_xuatRieng.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.checkBox_xuatRieng_Click);
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.editBox4);
+            this.group2.Items.Add(this.editBox5);
+            this.group2.Items.Add(this.checkBox1);
+            this.group2.Items.Add(this.button1);
+            this.group2.Label = "Xuất từ nhiều TK";
+            this.group2.Name = "group2";
+            // 
+            // editBox4
+            // 
+            this.editBox4.Enabled = false;
+            this.editBox4.Label = "List TK";
+            this.editBox4.Name = "editBox4";
+            this.editBox4.Text = null;
+            // 
+            // editBox5
+            // 
+            this.editBox5.Enabled = false;
+            this.editBox5.Label = "Packinglist";
+            this.editBox5.Name = "editBox5";
+            this.editBox5.Text = null;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Label = "Xuất file riêng";
+            this.checkBox1.Name = "checkBox1";
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Label = "Hóa đơn mẫu";
+            this.button5.Name = "button5";
+            this.button5.ShowImage = true;
+            this.button5.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Label = "Thư mục xuất";
+            this.button7.Name = "button7";
+            this.button7.ShowImage = true;
+            this.button7.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button7_Click);
+            // 
+            // button_invoice
+            // 
+            this.button_invoice.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button_invoice.Image = ((System.Drawing.Image)(resources.GetObject("button_invoice.Image")));
+            this.button_invoice.Label = "Invoice";
+            this.button_invoice.Name = "button_invoice";
+            this.button_invoice.ShowImage = true;
+            // 
+            // button_shipping
+            // 
+            this.button_shipping.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button_shipping.Image = ((System.Drawing.Image)(resources.GetObject("button_shipping.Image")));
+            this.button_shipping.Label = "Shipping";
+            this.button_shipping.Name = "button_shipping";
+            this.button_shipping.ShowImage = true;
+            this.button_shipping.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button_shipping_Click);
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Enabled = false;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Label = "Xuất toàn bộ";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab_xnk);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab_xnk.ResumeLayout(false);
+            this.tab_xnk.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group_xnk.ResumeLayout(false);
+            this.group_xnk.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab_xnk;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group_xnk;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button_invoice;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button_shipping;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox checkBox_xuatRieng;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox checkBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox_hoadonmau;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button7;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox_thumucxuat;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox checkBox_openAfter;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ribbon_xnk_sunzex/ribbon_xnk_sunzex/Ribbon1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Win32;
+
+namespace ribbon_xnk_sunzex
+{
+    public partial class Ribbon1
+    {
+        string sTemplatePath = "";
+        string sOutputPath = "";
+        private readonly string KEY_NAME_SUNZEX_templatePath = "SUNZEX_XNK_TEMPLATE_PATH";
+        private readonly string KEY_NAME_SUNZEX_outputPath = "SUNZEX_XNK_OUTPUT_PATH";
+        private readonly string KEY_NAME_SUNZEX_openAfter = "SUNZEX_XNK_OPEN_AFTER";
+        private readonly string KEY_NAME_SUNZEX_outSeparate = "SUNZEX_XNK_OUT_SEPARATE";
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            sTemplatePath = ReadFromRegistry(KEY_NAME_SUNZEX_templatePath, "");
+            editBox_hoadonmau.Text = sTemplatePath;
+
+            sOutputPath = ReadFromRegistry(KEY_NAME_SUNZEX_outputPath, "");
+            editBox_thumucxuat.Text = sOutputPath;
+
+            checkBox_openAfter.Checked = "1".Equals(ReadFromRegistry(KEY_NAME_SUNZEX_openAfter, "0"));
+
+            checkBox_xuatRieng.Checked = "1".Equals(ReadFromRegistry(KEY_NAME_SUNZEX_outSeparate, "0"));
+        }
+
+        private void button5_Click(object sender, RibbonControlEventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = editBox_hoadonmau.Text;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                editBox_hoadonmau.Text = folderBrowserDialog1.SelectedPath;
+                StoreInRegistry(KEY_NAME_SUNZEX_templatePath, editBox_hoadonmau.Text);
+                sTemplatePath = editBox_hoadonmau.Text;
+            }
+        }
+
+        public void StoreInRegistry(string keyName, string value)
+        {
+            RegistryKey rootKey = Registry.CurrentUser;
+            string registryPath = @"Software\Sunzex\XNK_ribbon";
+            using (RegistryKey rk = rootKey.CreateSubKey(registryPath))
+            {
+                rk.SetValue(keyName, value, RegistryValueKind.String);
+            }
+        }
+        public string ReadFromRegistry(string keyName, string defaultValue)
+        {
+            RegistryKey rootKey = Registry.CurrentUser;
+            string registryPath = @"Software\Sunzex\XNK_ribbon";
+            using (RegistryKey rk = rootKey.OpenSubKey(registryPath, false))
+            {
+                if (rk == null)
+                {
+                    return defaultValue;
+                }
+
+                var res = rk.GetValue(keyName, defaultValue);
+                if (res == null)
+                {
+                    return defaultValue;
+                }
+
+                return res.ToString();
+            }
+        }
+
+        private void button7_Click(object sender, RibbonControlEventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = editBox_thumucxuat.Text;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                editBox_thumucxuat.Text = folderBrowserDialog1.SelectedPath;
+                StoreInRegistry(KEY_NAME_SUNZEX_outputPath, editBox_thumucxuat.Text);
+                sOutputPath = editBox_thumucxuat.Text;
+            }
+        }
+
+        private void button_shipping_Click(object sender, RibbonControlEventArgs e)
+        {
+            Workbook wb = (Workbook)Globals.ThisAddIn.Application.ActiveWorkbook;
+            Worksheet worksheet = wb.ActiveSheet;
+
+            //Check
+            if (!"Số tờ khai".Equals(worksheet.Range["C4"].Value2) || !worksheet.Range["E2"].Value2.Contains("Tờ khai")) {
+                MessageBox.Show("Hãy mở tờ khai để tạo shipping");
+                return;
+            }
+            ShippingModel ship = new ShippingModel();
+            string a;
+            a = worksheet.Range["F8"].Value2; ship.date = a.Substring(0, 10);
+            a = worksheet.Range["I46"].Value2; ship.shipment = a.Substring(3, 3) + a.Substring(0, 3) + a.Substring(6, 4);
+            a = worksheet.Range["L6"].Value2;
+            ship.transport = "BY " + (("" + a[a.Length - 1]).Equals("3") || ("" + a[a.Length - 1]).Equals("2") ? "SEA" : ("" + a[a.Length - 1]).Equals("4") ? "TRUCK" : ("" + a[a.Length - 1]).Equals("1") ? "AIR" : "OTHER");
+            ship.destination = worksheet.Range["M43"].Value2;
+            ship.portload = worksheet.Range["M44"].Value2;
+            ship.voyage = worksheet.Range["M45"].Value2;
+            ship.comodity = "AS PER INVOICE NO: " + worksheet.Range["R49"].Value2;
+            ship.amount = worksheet.Range["U53"].Value2;
+
+            ship.total = worksheet.Range["H40"].Value2;
+            if (!"CT".Equals(worksheet.Range["M40"].Value2))
+            {
+                a = worksheet.Range["H47"].Value2;
+                if (a.LastIndexOf("=") < a.LastIndexOf(")") && a.LastIndexOf("C") < a.LastIndexOf("T") && a.LastIndexOf("=") < a.LastIndexOf("C") && a.LastIndexOf("T") < a.LastIndexOf(")"))
+                {
+                    ship.total = a.Substring(a.LastIndexOf("=") + 1, a.LastIndexOf("C") - a.LastIndexOf("=") - 1);
+                }
+            }
+
+            ship.gross = worksheet.Range["H41"].Value2;
+
+            if (checkBox_xuatRieng.Checked) {
+                ship.filename = @"SUNZEX_SHIPING." + worksheet.Range["R49"].Value2.Replace("/", "");
+                ship.sheetname = worksheet.Range["R49"].Value2.Replace("/", "");
+            } else {
+                a = ship.comodity.Substring(ship.comodity.IndexOf(@":") + 6, 2) + "." + ship.comodity.Substring(ship.comodity.IndexOf(@":") + 8, 3);
+                ship.sheetname = a.Replace("/", "");
+
+                ship.filename = @"SUNZEX_SHIPING.T" + ship.date.Substring(3, 2) + "." + ship.date.Substring(6, 4);
+            }
+            
+            Copy_shipping_work_sheet(ship);
+        }
+        private void Copy_shipping_work_sheet(ShippingModel ship)
+        {
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            try
+            {
+                string templatePath = sTemplatePath + @"\Sunzex_SHIP.xlsx";
+                string outputPath = sOutputPath + @"\" + ship.filename + ".xlsx";
+
+                app.Visible = false;
+                app.Workbooks.Add();
+                if (!System.IO.File.Exists(templatePath))
+                {
+                    MessageBox.Show("Không tìm thấy file mẫu");
+                    app.Workbooks.Close();
+                    app.Quit();
+                    return;
+                }
+                app.Workbooks.Add(templatePath);
+                try
+                {
+                    app.Workbooks.Add(!System.IO.File.Exists(outputPath) ? "" : outputPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                _Worksheet ws = (_Worksheet)app.Workbooks[2].Worksheets[1];
+                _Worksheet sheet = (_Worksheet)app.Workbooks[3].Worksheets[1];
+
+                //check exist
+                bool found = false;
+                foreach (Microsoft.Office.Interop.Excel._Worksheet aSheet in app.Workbooks[3].Sheets) {
+                    if (ship.sheetname.Equals(aSheet.Name)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    throw new IOException();
+                }
+
+                ws.Copy(sheet);
+                app.Workbooks[3].Sheets[1].Activate();
+                sheet = app.ActiveSheet;
+                //Modify
+                sheet.Range["H7"].Value2 = ship.date;
+                sheet.Range["E14"].Value2 = ship.shipment;
+                sheet.Range["E15"].Value2 = ship.transport;
+                sheet.Range["E16"].Value2 = ship.destination;
+                sheet.Range["E17"].Value2 = ship.portload;
+                sheet.Range["E18"].Value2 = ship.voyage;
+                sheet.Range["E19"].Value2 = ship.comodity;
+                sheet.Range["E20"].Value2 = ship.amount.Replace(".", "").Replace(",", ".");
+                sheet.Range["E22"].Value2 = ship.total.Replace(".", "");
+                sheet.Range["E23"].Value2 = ship.gross.Replace(".", "").Replace(",", ".");
+                sheet.Name = ship.sheetname;
+
+                app.ActiveWorkbook.SaveAs(outputPath);
+                app.Workbooks[3].Close();
+                app.Workbooks[2].Close();
+                app.Workbooks[1].Close();
+                app.Workbooks.Close();
+                app.Quit();
+                if (checkBox_openAfter.Checked) {
+                    OpenFile(outputPath);
+                } else MessageBox.Show("Shipping " + ship.sheetname + " tạo thành công!");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Shipping "+ship.sheetname+" đã tồn tại, không thể tạo mới! "+ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                app.Workbooks.Close();
+                app.Quit();
+            }
+        }
+        private void OpenFile(string path)
+        {
+            Process fileopener = new Process();
+            fileopener.StartInfo.FileName = "explorer";
+            fileopener.StartInfo.Arguments = "\"" + path + "\"";
+            fileopener.Start();
+        }
+        public class InvoiceModel
+        {
+            public int type;
+            public string number, date, consignee, portload, destination, sailing;
+            public string[] detail_name, detail_order, detail_PO, detail_quantity, detail_price;
+            public InvoiceModel(int type)
+            {
+                this.type = type;
+                detail_name = new string[3];
+                detail_order = new string[3];
+                detail_PO = new string[3];
+                detail_quantity = new string[3];
+                detail_price = new string[3];
+            }
+        }
+        public class ShippingModel
+        {
+            public string date, shipment, transport, destination, portload, voyage, comodity, amount, total, gross, filename, sheetname;
+            public ShippingModel() { }
+        }
+
+        private void CheckBox3_Click(object sender, RibbonControlEventArgs e)
+        {
+            StoreInRegistry(KEY_NAME_SUNZEX_openAfter, checkBox_openAfter.Checked ? "1" : "0");
+        }
+
+        private void checkBox_xuatRieng_Click(object sender, RibbonControlEventArgs e)
+        {
+            StoreInRegistry(KEY_NAME_SUNZEX_outSeparate, checkBox_xuatRieng.Checked ? "1" : "0");
+        }
+    }
+}
